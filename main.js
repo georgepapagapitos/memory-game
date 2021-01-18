2586e1a6e482a9687701b28f8df94d88109bf1d9
George Papagapitos <george.g.papagapitos@gmail.com>
2021-01-18 14:10:33 -0600

    added match function

--- main.js
+++ main.js
@@ -44,19 +44,51 @@ for(i = 0; i < gameGrid.length; i++) {
   grid.appendChild(card);
 }
 
+let firstGuess = '';
+let secondGuess = '';
+
+// set clickedCount to 0
 let clickedCount = 0;
 
+let previousTarget = null;
+
+// add match CSS
+let match = function() {
+  let selected = document.querySelectorAll('.selected');
+  // loop through the array like object containing 'selected' class
+  for(let i = 0; i < selected.length; i++) {
+    selected[i].classList.add('match');
+  }
+}
+ 
 // add event listener to grid
 grid.addEventListener('click', function(event) {
   // declare variable to target our clicked item
   let clicked = event.target;
   // do not allow the grid section to be selected
-  if(clicked.nodeName === 'SECTION') {
+  if(clicked.nodeName === 'SECTION' || clicked === previousTarget || clicked.parentNode.classList.contains('match') || clicked.parentNode.classList('selected')) {
     return;
   }
+  // add 'selected' class if the count is less than 2
   if(clickedCount < 2) {
     clickedCount++
-    // add selected class
-    clicked.classList.add('selected');
+    if(clickedCount === 1) {
+      // assign first guess
+      firstGuess = clicked.dataset.name;
+      clicked.classList.add('selected');
+    } else {
+      // assign second guess
+      secondGuess = clicked.dataset.name;
+      clicked.classList.add('selected');
+    }
+    // if both guesses are not empty
+    if(firstGuess !== '' && secondGuess !== '') {
+      // and firstGuess matches secondGuess
+      if(firstGuess === secondGuess) {
+        // run match funtion
+        match();
+      }
+    }
+    previousTarget = clicked;
   }  
 });
